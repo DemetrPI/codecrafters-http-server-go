cdd989f10dd8d30c859a1eaeefd09f3d7e23f520
DemetrPI <demetriy.78@gmail.com>
2025-08-13 17:10:46 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,26 +4,62 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
-func main() {
-
-	s := Server{}
-	s.Start()
-
-}
+const CRLF = "\r\n"
 
 type Server struct {
 	listener net.Listener
 }
 
-func (s *Server) Start() {
+func main() {
+
+	s := Server{}
 	s.Listen()
 	defer s.Close()
-	conn := s.Accept()
 
-	fmt.Println("Accepted connection from: ", conn.RemoteAddr())
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	for {
+		conn := s.Accept()
+		go s.HandleConnection(conn)
+	}
+}
+
+func (s *Server) HandleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	fmt.Println("Client connected from:", conn.RemoteAddr())
+
+	var buf = make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading: ", err.Error())
+		return
+	}
+
+	req := string(buf)
+	lines := strings.Split(req, CRLF)
+
+	requestLine := lines[0]
+	parts := strings.Split(requestLine, " ")
+	if len(parts) < 2 {
+		fmt.Println("Invalid request")
+		return
+	}
+	path := parts[1]
+
+	fmt.Println("Request path: ", path)
+
+	var response string
+	if path == "/" {
+		response = "HTTP/1.1 200 OK\r\n\r\n"
+	} else {
+		response = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+	_, err = conn.Write([]byte(response))
+	if err != nil {
+		fmt.Println("Error writing: ", err.Error())
+	}
 }
 
 func (s *Server) Listen() {
